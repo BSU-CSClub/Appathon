3f6db9220cfc8e8f023e77d3efc79db36ce5ad6a
Nathaniel Hernandez <Nathaniel@Nathaniels-MBP.boisestate.edu>
2016-03-05 15:18:23 -0700

    Readded MyUpvotes and MyPosts pages

--- Frontend/Source/Appathon/Appathon/ViewModels/AccountViewModel.cs
+++ Frontend/Source/Appathon/Appathon/ViewModels/AccountViewModel.cs
@@ -12,9 +12,15 @@ namespace Appathon.ViewModels
 	{
 		public ICommand LogoutCommand { get; set; }
 
+		public ICommand NavigateMyPostsCommand { get; set; }
+
+		public ICommand NavigateMyUpvotesCommand { get; set; }
+
 		public AccountViewModel ()
 		{
 			this.LogoutCommand = new Command (this.Logout);
+			this.NavigateMyPostsCommand = new Command (this.NavigateMyPosts);
+			this.NavigateMyUpvotesCommand = new Command (this.NavigateMyUpvotes);
 		}		
 
 		private void Logout() 
@@ -22,6 +28,16 @@ namespace Appathon.ViewModels
 			App.AppEventAggregator.GetEvent<SecurityResolverChangedEvent> ().Publish (new SecurityResolver ());
 			App.AppEventAggregator.GetEvent<NavigateToViewEvent> ().Publish (new LoginView ());
 			App.AppEventAggregator.GetEvent<ToggleMenuEvent> ().Publish (MenuToggleFlag.ToggleMenuToggleable);
+		}
+
+		private void NavigateMyPosts()
+		{
+			App.AppEventAggregator.GetEvent<NavigateToViewEvent> ().Publish (new MyPostsView ());
+		}
+
+		private void NavigateMyUpvotes()
+		{
+			App.AppEventAggregator.GetEvent<NavigateToViewEvent> ().Publish (new MyUpvotesView ());
 		}
 	}
 }
--- Frontend/Source/Appathon/Appathon/ViewModels/MyPostsViewModel.cs
+++ Frontend/Source/Appathon/Appathon/ViewModels/MyPostsViewModel.cs
@@ -24,4 +24,3 @@ namespace Appathon.ViewModels
 		}
 	}
 }
-
