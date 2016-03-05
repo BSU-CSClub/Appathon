7bf0d7c3b74d1afd9cea2e05ba29788b18c17c53
Kelson Ball <kelson.j.ball@gmail.com>
2016-03-05 08:14:16 -0700

    PostsView switched to ContentView

--- Frontend/Source/Appathon/Appathon/ViewModels/PostsViewModel.cs
+++ Frontend/Source/Appathon/Appathon/ViewModels/PostsViewModel.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Collections.Generic;
 
-namespace Appathon
+
+namespace Appathon.ViewModels
 {
 	using Models;
 
-	public class PostsViewModel : INotifyPropertyChanged
+	public class PostsViewModel : ViewModelBase
 	{
+		List<PostViewModel> Posts { get; set; }
+
 		public PostsViewModel ()
 		{
-		}
-
-
-		public event PropertyChangedEventHandler PropertyChanged;
+			this.Posts = new List<PostViewModel> ();
+			for (int i = 0; i < 5; i++) {
+				this.Posts.Add (new PostViewModel ());
+			}
+			App.AppEventAggregator.GetEvent<PostsUpdatedEvent> ().Publish (this.Posts);
+		}			
 
 	}
 }
--- Frontend/Source/Appathon/Appathon/Views/PostsView.xaml.cs
+++ Frontend/Source/Appathon/Appathon/Views/PostsView.xaml.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 using Xamarin.Forms;
 
 namespace Appathon
-{
-	public partial class PostsView : ContentPage
+{	
+	using ViewModels;
+
+	public partial class PostsView : ContentView
 	{
+		
 		public PostsView ()
 		{
 			InitializeComponent ();
+			App.AppEventAggregator.GetEvent<PostsUpdatedEvent> ().Subscribe (this.UpdatePostDisplay);
+			BindingContext = new PostsViewModel ();
+
+		}
+
+		private void UpdatePostDisplay(List<PostViewModel> posts)
+		{
+			this.Posts.ItemsSource = posts;
 		}
 	}
 }
